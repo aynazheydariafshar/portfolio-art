eec1dbad02a69e08c40f043388a95174dcbdccf8
aynazheydariafshar <aynazhadriafshar@gmail.com>
2023-07-20 19:02:03 +0330

    add animation for image on home page

--- app/page.js
+++ app/page.js
@@ -12,7 +12,7 @@ function Home() {
             alt="art_home_page"
             width={550}
             height={550}
-            className="w-full relative top-1/2 object-contain transform transition duration-1000"
+            className="animate-slidein w-full relative top-1/2 object-contain transform transition duration-1000"
           />
           <div className="absolute top-[60%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center w-full">
             <h1 className="text-5xl md:text-8xl lg:text-8xl  font-bold">
